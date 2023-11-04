5c63f896414cdb81946993dd7caabbef29134539
tynchtykkg <tynchtyk.estebesuulu@gmail.com>
2023-11-04 21:03:04 +0600

    first commit

---
+++ bin/set_map.dart
@@ -0,0 +1,5 @@
+import 'package:set_map/set_map.dart' as set_map;
+
+void main(List<String> arguments) {
+  print('Hello world: ${set_map.calculate()}!');
+}
---
+++ test/set_map_test.dart
@@ -0,0 +1,8 @@
+import 'package:set_map/set_map.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
